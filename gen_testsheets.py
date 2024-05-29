0cfd14a6aa5c884a99b64225df68747a2d94d11c
Brian Wheeler <bdwheele@iu.edu>
2024-05-29 16:32:15 -0400

    add a techniques sheet too

--- gen_testsheets.py
+++ gen_testsheets.py
@@ -32,6 +32,10 @@ def main():
     # get our test sheet template
     template = Template((Path(sys.path[0]) / "template.html").read_text())
 
+    # get the techniques sheet template
+    tech_template = Template((Path(sys.path[0]) / "techniques.html").read_text())
+
+
     # walk the ranks, generating each sheet as we go.
     for rank in ranks:
         print(f"Generating {ranks[rank]}")
@@ -39,52 +43,79 @@ def main():
 
         # generate the HTML tables for the data...
         html_tables = []
+        tech_tables = []
         for tdata in data['tables']:
-            html = """<table class="ttable">\n"""
+            thtml = html = """<table class="ttable">\n"""
             title = tdata['title']
             if tdata['subtitle']:
                 # append the subtitle
                 title += f"""</br><span class="subtitle">{tdata['subtitle']}</span>"""
             html += f"""  <caption>{title}</caption>\n"""
+            thtml += f"""  <caption>{title}</caption>\n"""
             html += f"""  <colgroup><col class="narrow"/><col class="narrow"/><col/></colgroup>\n"""
+            thtml += f"""  <colgroup><col class="narrow"/><col/></colgroup>\n"""
             html += f"""  <tr><td>&nbsp;</td><th>Score</th><th>Comments</th></tr>\n"""
+            thtml += f"""  <tr><td>&nbsp;</td><th>Notes</th></tr>\n"""
             for header in tdata['headers']:
                 if not header['techniques']:
                     # skip empty headers
                     continue
                 if header['label'] != '':
                     html += f"""  <tr><td class="theader">{nbsp(header['label'])}</td><td></td><td></td></tr>\n"""
+                    thtml += f"""  <tr><td class="theader">{nbsp(header['label'])}</td><td></td></tr>\n"""
                 header_rows = 0
                 for t in header['techniques']:
+                    label = nbsp(t['label'])
+                    if t['type'] == 'N':
+                        # new technique
+                        label = f'<span class="new">{label}</span>'
+                    elif t['type'] != 'X':
+                        label = f'{label} ({t["type"]})'
+
                     if args.full or header['type'] == 'X' or (header['type'] == 'C' and t['type'] != 'X'):
                         # either show all techniques or only show those that 
                         # are new or have a count
-                        label = nbsp(t['label'])
-                        if t['type'] == 'N':
-                            # new technique
-                            label = f'<span class="new">{label}</span>'
-                        elif t['type'] != 'X':
-                            label = f'{label} ({t["type"]})'
                         html += f"  <tr><td>{label}</td><td></td><td></td></tr>\n"
                         header_rows += 1
-                
+                        
+                    thtml += f"  <tr><td>{label}</td><td></td></tr>\n"
+                                        
                 if header['type'] == 'C' and header_rows == 0:
                     # add a blank row for comments.
                     html += "  <tr><td>&nbsp;</td><td></td><td></td></tr>\n"
+                    thtml += "  <tr><td>&nbsp;</td><td></td></tr>\n"
 
             html += """</table>"""
+            thtml += """</table>"""
             html_tables.append(html)
+            tech_tables.append(thtml)
 
+
+
+
+        # generate the test sheet
         html_file = f"{outdir}/{ranks[rank][0]}.html"
         with open(html_file, "w") as f:
             f.write(template.safe_substitute(title=ranks[rank][1], 
                                              tables='\n'.join(html_tables),
                                              revision=data['revision']))
-
         # generate the word doc
         subprocess.run(['pandoc', html_file, "-o", f"{outdir}/{ranks[rank][0]}.docx"])
         # generate the pdf
         subprocess.run(['weasyprint', html_file, f"{outdir}/{ranks[rank][0]}.pdf"])
+
+        # generate the technique list
+        html_file = f"{outdir}/{ranks[rank][0]}-techniques.html"
+        with open(html_file, "w") as f:
+            f.write(tech_template.safe_substitute(title=ranks[rank][1], 
+                                             tables='\n'.join(tech_tables),
+                                             revision=data['revision']))
+        # generate the word doc
+        subprocess.run(['pandoc', html_file, "-o", f"{outdir}/{ranks[rank][0]}-techniques.docx"])
+        # generate the pdf
+        subprocess.run(['weasyprint', html_file, f"{outdir}/{ranks[rank][0]}-techniques.pdf"])
+
+
 
 
 def nbsp(text):
