4a19c30a1d1cb0ee39539a1f9a226aea8e5232e9
Brian Wheeler <bdwheele@iu.edu>
2024-06-04 10:00:41 -0400

    cleanup code

--- gen_testsheets.py
+++ gen_testsheets.py
@@ -24,101 +24,133 @@ ranks = {
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--full", default=False, action="store_true", help="Don't collapse headers")
+    parser.add_argument("--full", default=False, action="store_true", help="Don't collapse headers")    
     args = parser.parse_args()
 
     outdir = sys.path[0] + "/test_sheets"
+
     # get our test sheet template
     template = Template((Path(sys.path[0]) / "test_template.html").read_text())
 
     # get the techniques sheet template
     tech_template = Template((Path(sys.path[0]) / "techniques_template.html").read_text())
 
-    # walk the ranks, generating each sheet as we go.
+
+    # walk the ranks, generating each sheet as we go.    
     for rank in ranks:
         print(f"Generating {ranks[rank]}")
+
+        # read the csv inventory
         data = read_inventory(Path(sys.path[0]) / "inventory.csv", rank)
 
-        # generate the HTML tables for the data...
-        html_tables = []
-        tech_tables = []
-        for tdata in data['tables']:
-            html = """<table class="ttable">\n"""
+        for sheet in [('test_template.html', gen_test_content, '-test'),
+                      ('techniques_template.html', gen_tech_content, '-techniques')]:
+            content = sheet[1](data, args.full)
+            filebase = ranks[rank][0] + sheet[2]
+            template = Template((Path(sys.path[0], sheet[0]).read_text()))
+            with open(f"{outdir}/{filebase}.html", "w") as f:
+                f.write(template.safe_substitute(title=ranks[rank][1],
+                                                 content=content,
+                                                 revision=data['revision']))
             
-            title = tdata['title']
-            if tdata['subtitle']:
-                # append the subtitle
-                title += f"""</br><span class="subtitle">{tdata['subtitle']}</span>"""
-            html += f"""  <caption>{title}</caption>\n"""
-            tech_html = f"""  <span class="title">{title}</span>\n"""
-            tech_html += """<ul>\n"""
-            html += f"""  <colgroup><col class="narrow"/><col class="narrow"/><col/></colgroup>\n"""
-            #tech_html += f"""  <colgroup><col class="narrow"/><col/></colgroup>\n"""
-            html += f"""  <tr><td>&nbsp;</td><th>Score</th><th>Comments</th></tr>\n"""
-            #tech_html += f"""  <tr><td>&nbsp;</td><th>Notes</th></tr>\n"""
-            for header in tdata['headers']:
-                if not header['techniques']:
-                    # skip empty headers
-                    continue
-                if header['label'] != '':
-                    html += f"""  <tr><td class="theader">{nbsp(header['label'])}</td><td></td><td></td></tr>\n"""
-                    #tech_html += f"""  <tr><td class="theader">{nbsp(header['label'])}</td><td></td></tr>\n"""
-                    tech_html += f"""  <li class="section">{nbsp(header['label'])}</li>\n"""
-                header_rows = 0
-                for t in header['techniques']:
-                    label = nbsp(t['label'])
-                    if t['type'] == 'N':
-                        # new technique
-                        label = f'<span class="new">{label}</span>'
-                    elif t['type'] != 'X':
-                        label = f'{label} ({t["type"]})'
-
-                    if args.full or header['type'] == 'X' or (header['type'] == 'C' and t['type'] != 'X'):
-                        # either show all techniques or only show those that 
-                        # are new or have a count
-                        html += f"  <tr><td>{label}</td><td></td><td></td></tr>\n"
-                        header_rows += 1
-                        
-                    tech_html += f'  <li class="technique">{label}</li>\n'
-                                        
-                if header['type'] == 'C' and header_rows == 0:
-                    # add a blank row for comments.
-                    html += "  <tr><td>&nbsp;</td><td></td><td></td></tr>\n"
-                    #tech_html += "  <tr><td>&nbsp;</td><td></td></tr>\n"
-
-            html += """</table>"""
-            tech_html += """</ul>"""
-            html_tables.append(html)
-            tech_tables.append(tech_html)
+            # generate the word doc & pdf
+            subprocess.run(['pandoc', f'{outdir}/{filebase}.html', "-o", f"{outdir}/{filebase}.docx"])
+            subprocess.run(['weasyprint', f'{outdir}/{filebase}.html', f"{outdir}/{filebase}.pdf"])
 
 
+def gen_test_content(data, full=False):
+    """Generate the content for a test sheet"""
+    html_tables = []
+    for tdata in data['tables']:
+        html = """<table class="ttable">\n"""
+        
+        title = tdata['title']
+        if tdata['subtitle']:
+            # append the subtitle
+            title += f"""</br><span class="subtitle">{tdata['subtitle']}</span>"""
+        html += f"""  <caption>{title}</caption>\n"""
+        html += f"""  <colgroup><col class="narrow"/><col class="narrow"/><col/></colgroup>\n"""
+        html += f"""  <tr><td>&nbsp;</td><th>Score</th><th>Comments</th></tr>\n"""
+        for header in tdata['headers']:
+            if not header['techniques']:
+                # skip empty headers
+                continue
+            if header['label'] != '':
+                html += f"""  <tr><td class="theader">{nbsp(header['label'])}</td><td></td><td></td></tr>\n"""
+            header_rows = 0
+            for t in header['techniques']:
+                label = nbsp(t['label'])
+                if t['type'] == 'N':
+                    # new technique
+                    label = f'<span class="new">{label}</span>'
+                elif t['type'] != 'X':
+                    label = f'{label} ({t["type"]})'
+
+                if full or header['type'] == 'X' or (header['type'] == 'C' and t['type'] != 'X'):
+                    # either show all techniques or only show those that 
+                    # are new or have a count
+                    html += f"  <tr><td>{label}</td><td></td><td></td></tr>\n"
+                    header_rows += 1
+            if header['type'] == 'C' and header_rows == 0:
+                # add a blank row for comments.
+                html += "  <tr><td>&nbsp;</td><td></td><td></td></tr>\n"
+
+        html += """</table>"""
+        html_tables.append(html)
+    
+    return "\n".join(html_tables)
 
 
-        # generate the test sheet
-        html_file = f"{outdir}/{ranks[rank][0]}.html"
-        with open(html_file, "w") as f:
-            f.write(template.safe_substitute(title=ranks[rank][1], 
-                                             tables='\n'.join(html_tables),
-                                             revision=data['revision']))
-        # generate the word doc
-        subprocess.run(['pandoc', html_file, "-o", f"{outdir}/{ranks[rank][0]}.docx"])
-        # generate the pdf
-        subprocess.run(['weasyprint', html_file, f"{outdir}/{ranks[rank][0]}.pdf"])
+def gen_tech_content(data, full=False):
+    "generate technique sheet content"    
+    tech_tables = []
+    for tdata in data['tables']:        
+        title = tdata['title']
+        if tdata['subtitle']:
+            # append the subtitle
+            title += f"""</br><span class="subtitle">{tdata['subtitle']}</span>"""        
+        tech_html = f"""  <span class="title">{title}</span>\n"""
+        tech_html += """<ul>\n"""        
+        for header in tdata['headers']:
+            if not header['techniques']:
+                # skip empty headers
+                continue
+            if header['label'] != '':
+                tech_html += f"""  <li class="section">{nbsp(header['label'])}</li>\n"""
+            for t in header['techniques']:
+                label = nbsp(t['label'])
+                if t['type'] == 'N':
+                    # new technique
+                    label = f'<span class="new">{label}</span>'
+                elif t['type'] != 'X':
+                    label = f'{label} ({t["type"]})'
 
-        # generate the technique list
-        html_file = f"{outdir}/{ranks[rank][0]}-techniques.html"
-        with open(html_file, "w") as f:
-            f.write(tech_template.safe_substitute(title=ranks[rank][1], 
-                                             tables='\n'.join(tech_tables),
-                                             revision=data['revision']))
-        # generate the word doc
-        subprocess.run(['pandoc', html_file, "-o", f"{outdir}/{ranks[rank][0]}-techniques.docx"])
-        # generate the pdf
-        subprocess.run(['weasyprint', html_file, f"{outdir}/{ranks[rank][0]}-techniques.pdf"])
+                tech_html += f'  <li class="technique">{label}</li>\n'
+                                    
+        tech_html += """</ul>"""
+        tech_tables.append(tech_html)
+
+    return "\n".join(tech_tables)
 
 
 def nbsp(text):
+    "Force any spaces to non-breaking spaces"
     return text.replace(' ', '&nbsp;')
+
+def fix_text(text):
+    "Fix any html-special codes and/or things excel put in"
+    # special character table
+    special = {'&': '&amp;',
+               '<': '&lt;',
+               '>': '&gt;',
+               '’': "'",
+               '"': '"',
+               '“': '"'}
+    for q, r in special.items():
+        text = text.replace(q, r)
+
+    return text
+
 
 
 def read_inventory(invfile, rank):        
@@ -129,7 +161,7 @@ def read_inventory(invfile, rank):
         rdr = csv.DictReader(csvfile)
         for row in rdr:
             # make sure that leading/trailing whitespace is stripped...
-            row = {k: v.strip() for k, v in row.items()}
+            row = {k: fix_text(v.strip()) for k, v in row.items()}
             if row['Type'] == 'R':
                 # this is the test sheet revision
                 revision = row['Label']
